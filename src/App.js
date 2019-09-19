844d0f894b78c8ac23cff6f7a0266f02871e772c
Jessica <jessica@hiclark.com>
2019-09-19 12:44:54 -0400

    testing components using testing lib

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { makeGrid } from './utils';
 import './App.css';
 
 const COLS = 30;
 const ROWS = 30;
 
+export const Cell = ({ isAlive, x, y }) => {
+  return (
+    <div className="item" data-testid="item">
+      {isAlive ? '*' : ''}
+    </div>
+  );
+};
+
+export const Grid = ({ grid }) => {
+  return grid.reverse().map(({ isAlive, x, y }) => (
+    <Cell
+      isAlive={isAlive}
+      key={`${x}-${y}`}
+      x={x}
+      y={y}
+    />
+  ));
+};
+
 function App() {
-  const grid = makeGrid(COLS, ROWS)
+  const [grid, setGrid] = useState(makeGrid(COLS, ROWS));
 
   return (
     <div className="App">
-      <div className="grid-container">
-        {
-          grid.reverse().map(({ isAlive, x, y }) => {
-            return (
-              <div className="item">{isAlive ? '*' : null}</div>
-            );
-          })
-        }
+      <div className="grid-container" data-testid="grid">
+        <Grid grid={grid} />
       </div>
     </div>
   );
--- src/App.test.js
+++ src/App.test.js
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { render, cleanup } from '@testing-library/react';
 import '@testing-library/jest-dom';
-import App from './App';
+import App, { Cell, Grid } from './App';
+import { makeGrid } from './utils';
 
 afterEach(cleanup);
 
@@ -16,5 +17,32 @@ describe('App', () => {
   it('should match snapshot', () => {
     const { asFragment } = render(<App />);
     expect(asFragment()).toMatchSnapshot();
+  });
+
+  it('should render a grid', () => {
+    const { getByTestId } = render(<App />);
+    expect(getByTestId(/grid/i)).toBeTruthy();
+  });
+});
+
+describe('Grid', () => {
+  it('should render correctly', () => {
+    const grid = makeGrid(5, 5);
+    const { getAllByTestId } = render(<Grid grid={grid} />);
+    expect(getAllByTestId(/item/i)).toHaveLength(25);
+  });
+});
+
+describe('Cell', () => {
+  it('should render correctly', () => {
+    const props = { x: 0, y: 3, isAlive: true };
+    const { getByTestId } = render(<Cell {...props} />);
+    expect(getByTestId(/item/i).innerHTML).toBe('*');
+  });
+
+  it('should render correctly', () => {
+    const props = { x: 0, y: 3, isAlive: false };
+    const { getByTestId } = render(<Cell {...props} />);
+    expect(getByTestId(/item/i).innerHTML).toBe('');
   });
 });
