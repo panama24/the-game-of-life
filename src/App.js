0fc71b2b425e11d99c8e9c9bbb328edcc5bffb3a
Jessica <jessica@hiclark.com>
2019-09-19 12:03:56 -0400

    renders grid

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
 import React from 'react';
+import { makeGrid } from './utils';
+import './App.css';
+
+const COLS = 30;
+const ROWS = 30;
 
 function App() {
+  const grid = makeGrid(COLS, ROWS)
+
   return (
     <div className="App">
+      <div className="grid-container">
+        {
+          grid.reverse().map(({ isAlive, x, y }) => {
+            return (
+              <div className="item">{isAlive ? '*' : null}</div>
+            );
+          })
+        }
+      </div>
     </div>
   );
 }
