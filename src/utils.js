244b5cfa795991f56d11e7c6567e52e7f8932d68
Jessica <jessica@hiclark.com>
2019-09-19 11:24:07 -0400

    refactor

---
+++ src/utils.js
@@ -0,0 +1,71 @@
+const trueOrFalse = () => !!Math.floor(Math.random() * 2);
+const generateRandArray = () => Array(10).fill(null).map(c => trueOrFalse());
+
+const generateGrid = () => Array(10)
+  .fill(generateRandArray())
+  .map(arr => generateRandArray());
+
+const getCount = (array) => array.reduce((acc, { isAlive }) => {
+  if (isAlive) {
+    acc += 1
+  }
+  return acc;
+}, 0);
+
+const cell = (x, y, isAlive) => ({ x, y, isAlive });
+
+const makeGrid = (cols, rows) => {
+  let cells = [];
+
+  for (let i = 0; i < cols; i++) {
+    for (let j = 0; j < rows; j++) {
+      const cellX = i;
+      const cellY = j;
+      const isAlive = trueOrFalse();
+      cells.push(cell(cellX, cellY, isAlive));
+    }
+  }
+
+  return cells;
+}
+
+const getCoords = (x, y) => [
+  { x, y: y+1 },
+  { x, y: y-1 },
+  { x: x+1, y: y+1 },
+  { x: x+1, y },
+  { x: x+1, y: y-1 },
+  { x: x-1, y: y+1 },
+  { x: x-1, y },
+  { x: x-1, y: y-1 },
+];
+
+const getNeighbors = (grid, cell) => {
+  const coords = getCoords(cell.x, cell.y);
+
+  let neighbors = [];
+  for (let i = 0; i < grid.length; i++) {
+    for (let j = 0; j < coords.length; j++) {
+      if (grid[i]['x'] === coords[j]['x'] && grid[i]['y'] === coords[j]['y']) {
+        neighbors.push(grid[i])
+      }
+    }
+  }
+  return neighbors;
+}
+
+const shouldLive = (cell, neighbors) => {
+  const count = getCount(neighbors);
+  return cell.isAlive
+    ? (count === 2 || count === 3)
+    : count === 3;
+};
+
+export {
+  cell,
+  getCount,
+  getNeighbors,
+  makeGrid,
+  shouldLive,
+  trueOrFalse,
+};
---
+++ src/utils.test.js
@@ -0,0 +1,141 @@
+import {
+  cell,
+  getCount,
+  getNeighbors,
+  makeGrid,
+  shouldLive,
+  trueOrFalse,
+} from './utils';
+
+/* RULES:
+ * Alive cell - if fewer than 2 alive neighbors, dies due to underpopulation)
+ * Alive cell - 2 or 3 neighbors, lives due to perfect situation
+ * Alive cell - more than 3 alive neighbors, dies due to overpopulation
+ * Dead cell - exactly 3 alive neighbors, becomes alive due to reproduction
+ */
+
+describe('cell', () => {
+  it('should have properties x, y and isAlive', () => {
+    const { x, y, isAlive } = cell(0, 0, true);
+    expect(x).toEqual(0);
+    expect(y).toEqual(0);
+    expect(isAlive).toEqual(true);
+  });
+});
+
+describe('makeGrid', () => {
+  it('should generate grid of specified dimensions', () => {
+    const cols = 5;
+    const rows = 5;
+    const grid = makeGrid(cols, rows);
+    expect(grid.length).toEqual(cols * rows);
+  });
+});
+
+describe('trueOrFalse', () => {
+  it('chooses a random boolean', () => {
+    const res = [...Array(10)]
+      .fill(null)
+      .map(el => trueOrFalse());
+    const expectation = res.every(el => el);
+    expect(expectation).toBe(false);
+  });
+});
+
+describe('getCount', () => {
+  it('should count instances of true', () => {
+    const array = [
+      {x:0, y:0, isAlive: true},
+      {x:1, y:0, isAlive: true},
+      {x:2, y:0, isAlive: false},
+      {x:3, y:0, isAlive: false},
+      {x:4, y:0, isAlive: true},
+    ];
+    const expectation = getCount(array);
+    expect(expectation).toBe(3);
+  });
+});
+
+describe('shouldLive', () => {
+  describe('when cell is alive', () => {
+    it('should return true when there are 2-3 living neighbors', () => {
+      const neighbors = [
+        {x:0, y:0, isAlive: true},
+        {x:1, y:0, isAlive: true},
+        {x:2, y:0, isAlive: false},
+        {x:3, y:0, isAlive: false},
+        {x:4, y:0, isAlive: true},
+      ];
+      const res = shouldLive(true, neighbors);
+      expect(res).toBe(true);
+    });
+
+    it('should return false when there are fewer than 2 living neighbors', () => {
+      const neighbors = [
+        {x:0, y:0, isAlive: false},
+        {x:1, y:0, isAlive: false},
+        {x:2, y:0, isAlive: false},
+        {x:3, y:0, isAlive: false},
+        {x:4, y:0, isAlive: true},
+      ];
+      const res = shouldLive(true, neighbors);
+      expect(res).toBe(false);
+    });
+
+    it('should return false when there are more than 3 living neighbors', () => {
+      const neighbors = [
+        {x:0, y:0, isAlive: true},
+        {x:1, y:0, isAlive: true},
+        {x:2, y:0, isAlive: true},
+        {x:3, y:0, isAlive: false},
+        {x:4, y:0, isAlive: true},
+      ];
+      const res = shouldLive(true, neighbors);
+      expect(res).toBe(false);
+    });
+  });
+
+  describe('when cell is dead', () => {
+    it('should return true when there exactly 3 living neighbors', () => {
+      const neighbors = [
+        {x:0, y:0, isAlive: false},
+        {x:1, y:0, isAlive: true},
+        {x:2, y:0, isAlive: true},
+        {x:3, y:0, isAlive: false},
+        {x:4, y:0, isAlive: true},
+      ];
+      const res = shouldLive(false, neighbors);
+      expect(res).toBe(true);
+    });
+  });
+});
+
+describe('getNeighbors', () => {
+  describe('when cell is a middle cell', () => {
+    it('should return 8 neighboring cells', () => {
+      const cell = {x:2, y:2, isAlive: true};
+      const grid = makeGrid(5, 5);
+      const res = getNeighbors(grid, cell);
+      expect(res.length).toEqual(8);
+    });
+  });
+
+  describe('when cell is a corner cell', () => {
+    it('should return 3 neighboring cells', () => {
+      const cell = {x:0, y:0, isAlive: true};
+      const grid = makeGrid(5, 5);
+      const res = getNeighbors(grid, cell);
+      expect(res.length).toEqual(3);
+    });
+  });
+
+  describe('when cell is on the perimeter, but not a corner', () => {
+    it('should return 5 neighboring cells', () => {
+      const cell = {x:1, y:0, isAlive: true};
+      const grid = makeGrid(5, 5);
+      const res = getNeighbors(grid, cell);
+      expect(res.length).toEqual(5);
+    });
+  });
+});
+
